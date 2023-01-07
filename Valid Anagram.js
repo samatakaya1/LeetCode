285e2cedbfaf4c35669a7ed1466171bdaac70a7d
ilya.lipach <iplipachev@gmail.com>
2023-01-07 17:58:17 +0300

    Move Zeroes / Palindrome Linked List / Valid Anagram

---
+++ Valid Anagram.js
@@ -0,0 +1,25 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) =>
+  s.split("").sort().join("") === t.split("").sort().join("");
